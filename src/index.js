3f9fc2c7a0d7a7b31a4a2e7d0bd4c84869c7969c
Aaron Vickers <aaron_vickers@hotmail.co.uk>
2021-08-27 12:41:12 +0100

    Update to v1.1.0

--- src/index.js
+++ src/index.js
@@ -1,178 +1,82 @@
-const fs = require("fs")
+// Dependencies
 const express = require("express")
-const rbx = require("noblox.js")
+const noblox = require("noblox.js")
+const path = require("path")
+const fs = require("fs")
 
+// Utilities
+const account = require("./util/account.js")
+
+// Constants
+const PORT = process.env.PORT || 8080
+const COOKIE = process.env.COOKIE || ""
+const COOKIE_FILE = path.join(__dirname, "cookie.json")
+const COOKIE_REFRESH_INTERVAL = (1 * 1000 * 60 * 60 * 24) * 0.5
+
+// Express initialisation
 const app = express()
 app.use(express.json())
+app.use(require("./routes/routes.js"))
 
-const port = process.env.PORT || 8080
-const serverKey = process.env.SERVERKEY || ""
-const cookie = process.env.COOKIE || ""
-const productCacheName = "./products.json"
+const refreshCookieLoop = async (initialCookie) => {
+  fs.readFile(COOKIE_FILE, (err, data) => {
+    var cookieToRefresh = initialCookie
+    var initialRefreshDelay = COOKIE_REFRESH_INTERVAL
 
-const cookieFile = "./cookie.json"
-const cookieRefreshInterval = (1*1000*60*60*24)*0.5
+    if (err) {
+      console.warn(`Failed to read cookie file to get last updated time. Error: ${err.message}`)
+    } else {
+      const cookieJSON = JSON.parse(data)
+      const timeSinceRefresh = Date.now() - cookieJSON.time
 
-app.post("/requestProduct", function(req, res, next) {
-  const key = req.body.serverKey
-  
-  if (key !== serverKey) {
-    console.log("Invalid server key supplied.")
+      initialRefreshDelay = COOKIE_REFRESH_INTERVAL - timeSinceRefresh
+    }
 
-    return res.status(403).json({
-        error: "You do not have permission to use this."
-    })
-  }
-  
-  const universeId = parseInt(req.body.universeId)
-  const name = req.body.name
-  const price = parseInt(req.body.price)
-  
-  var productCache = require(productCacheName)
-  var productId = productCache[price.toString()]
-  
-  if (productId) {
-    return res.status(200).json({
-      message: "["+universeId+"] Product found "+name+" for "+price+" Robux.",
-      productId: parseInt(productId)
-    })
-  }
-  
-  const args = {
-    universeId: universeId,
-    name: name,
-    priceInRobux: price
-  }
+    const doCookieRefresh = async () => {
+      account.cookieRefresh(cookieToRefresh).then((refreshedCookie) => {
+        console.log(`Cookie has been refreshed.`)
 
-  rbx.addDeveloperProduct(args).then(function(productDetails) {
-    productId = productDetails.productId
+        cookieToRefresh = refreshedCookie
+      }).catch((err) => {
+        console.warn(`Failed to refresh cookie. Error: ${err.message}`)
+      })
+    }
 
-    productCache[price.toString()] = parseInt(productId)
-
-    fs.writeFile(productCacheName, JSON.stringify(productCache, null, 2), function(err) {
-      if (err) {
-        console.log("["+universeId+"] Failed to save product "+name+" for "+price+" Robux to file: "+err.message)
-      }
-    })
-
-    console.log("["+universeId+"] Created product "+name+" for "+price+" Robux.")
-
-    return res.status(200).json({
-      message: "["+universeId+"] Created product "+name+" for "+price+" Robux.",
-      productId: parseInt(productId)
-    })
-  }).catch(function(err) {
-    console.log("["+universeId+"] Failed to create product "+name+" for "+price+" Robux: "+err.message)
-
-    return res.status(400).json({
-      error: err.message
-    })
+    setTimeout(() => {
+      doCookieRefresh()
+      setInterval(doCookieRefresh, COOKIE_REFRESH_INTERVAL);
+    }, initialRefreshDelay);
   })
+}
+
+const initialiseApp = async () => {
+  const [loginSuccess, loginResult] = await account.cookieLogin(COOKIE).then((validCookie) => {
+    return [true, validCookie]
+  }).catch((err) => {
+    return [false, err.message]
+  })
+
+  if (!loginSuccess) {
+    throw new Error(`Failed to log in. Error: ${loginResult}`)
+  }
+
+  refreshCookieLoop(loginResult)
+  
+  noblox.getCurrentUser({
+    option: "UserName"
+  }).then((username) => {
+    console.log(`Logged in as ${username}`)
+  }).catch((err) => {
+    console.error(`Failed to get details of authenticated user. Error: ${err.message}`)
+  })
+
+  const server = app.listen(PORT)
+
+  return server
+}
+
+initialiseApp().then(async (server) => {
+  console.log(`Listening on port ${PORT}`)
+}).catch((err) => {
+  console.error(err.message)
 })
-
-app.get("/*", function(req, res, next) {
-  return res.status(200).json({})
-})
-
-async function cookieRefresh(useCookie, initialTime) {
-    setTimeout(async function() {
-        var newCookie = useCookie
-
-        try {
-            newCookie = await rbx.refreshCookie(useCookie)
-
-            fs.writeFile(cookieFile, JSON.stringify({
-                cookie: newCookie,
-                time: Date.now()
-            }), function(err) {
-                if (err) {
-                    console.error(`Failed to write refreshed cookie: ${err}`)
-                }
-                
-                return true
-            })
-        } catch(err) {
-            console.log(`Cookie refresh failed. Error: ${err}`)
-        }
-        
-        await cookieRefresh(newCookie)
-    }, initialTime || cookieRefreshInterval)
-}
-
-async function cookieLogin(defaultCookie) {
-    if (fs.existsSync(cookieFile)) {
-        const cookieJSON = JSON.parse(fs.readFileSync(cookieFile))
-        const savedCookie = cookieJSON.cookie
-        const timeSinceRefresh = Date.now()-cookieJSON.time
-        
-        try {
-            const isValidCookie = await rbx.setCookie(savedCookie)
-            
-            if (isValidCookie) {
-                await cookieRefresh(savedCookie, cookieRefreshInterval-timeSinceRefresh)
-                
-                return
-            } else {
-               throw new Error("Invalid or expired")
-            }
-        } catch(err) {
-            console.error(`Use of stored cookie failed: ${err}. Trying with supplied cookie.`)
-        }
-    }
-    
-    if (defaultCookie) {
-        try {
-            const isValidCookie = await rbx.setCookie(defaultCookie)
-            
-            if (isValidCookie) {
-                fs.writeFile(cookieFile, JSON.stringify({
-                    cookie: defaultCookie,
-                    time: Date.now()
-                }), function(err) {
-                    if (err) {
-                        console.error(`Failed to write cookie: ${err}`)
-                    }
-                    
-                    return true
-                })
-                
-                await cookieRefresh(defaultCookie)
-                
-                return
-            } else {
-               throw new Error("Invalid or expired")
-            }
-        } catch(err) {
-            console.error(`Use of supplied cookie failed: ${err}`)
-        }
-    }
-    
-    throw new Error("Cookie login failed, supplied and stored cookies are either missing or invalid.")
-}
-
-function logIn(newCookie) {
-    cookieLogin(newCookie).then(function() {
-        rbx.getCurrentUser({option: "UserName"}).then(function(username) {
-            console.log(`Logged in as ${username}`)
-        })
-        
-        console.log(`Listening on port ${port}`)
-        
-        return app.listen(port)
-    }).catch(function(err) {
-        console.error(`Failed to log in. Error: ${err.message}`)
-        
-        const errorApp = express()
-        errorApp.use(express.json())
-        
-        errorApp.get("/*", function(req, res, next) {
-            res.status(503).json({
-                error: `Failed to log in. Error: ${err.message}`
-            })
-        })
-        
-        return errorApp.listen(port)
-    })
-}
-
-const server = logIn(cookie)
---
+++ src/util/products.js
@@ -0,0 +1,55 @@
+// Dependencies
+const noblox = require("noblox.js")
+const path = require("path")
+const fs = require("fs")
+
+// Constants
+const PRODUCT_CACHE_FILE = path.join(__dirname, "..", "products.json")
+
+exports.getCachedProduct = async (price) => {
+  const productCache = require(PRODUCT_CACHE_FILE)
+  const cachedProductId = productCache[price.toString()]
+
+  if (cachedProductId !== undefined) {
+    return cachedProductId
+  }
+
+  throw new Error("Cached product doesn't exist")
+}
+
+exports.createNewProduct = async (universeId, name, price, overrideCache = false) => {
+  if (overrideCache !== true) {
+    const [cachedProductExists, cachedProductResult] = await exports.getCachedProduct(price).then((cachedProductId) => {
+      return [true, cachedProductId]
+    }).catch((err) => {
+      return [false, err.message]
+    })
+
+    if (cachedProductExists) {
+      throw new Error(`Cached product already exists`)
+    }
+  }
+
+  const [newProductCreated, newProductResult] = await noblox.addDeveloperProduct(universeId, name, price).then((productDetails) => {
+    const newProductId = parseInt(productDetails.productId)
+
+    var productCache = require(PRODUCT_CACHE_FILE)
+    productCache[price.toString()] = newProductId
+
+    fs.writeFile(PRODUCT_CACHE_FILE, JSON.stringify(productCache, null, 2), (err) => {
+      if (err) {
+        console.warn(`[${universeId}] Failed to save product ${name} for ${price} Robux to file. Error: ${err.message}`)
+      }
+    })
+
+    return [true, newProductId]
+  }).catch((err) => {
+    return [true, err.message]
+  })
+
+  if (newProductCreated) {
+    return newProductResult
+  }
+
+  throw new Error(newProductResult)
+}
